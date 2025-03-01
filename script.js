548284adebf6d358b3862ac65a4df271dea9d55e
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-01 11:32:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let tableE1=document.getElementById('sampleTable')
 function insert_Row() {
-    //Write your code here
+    var row=tableE1.insertRow(0)
+	var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+	cell1.innerHTML="New Cell1";
+	cell2.innerHTML="New Cell2";
   
   
 }
